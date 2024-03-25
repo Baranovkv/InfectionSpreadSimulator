4f60df5ffc393c372f38b80d40c927a47ba196e1
Kirill <K-baranov@outlook.com>
2024-03-26 00:23:57 +0100

    Initial Commit

---
+++ InfectionSpreadSimulator/InfectionSpreadSimulatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  InfectionSpreadSimulatorApp.swift
+//  InfectionSpreadSimulator
+//
+//  Created by Kirill Baranov on 26/03/24.
+//
+
+import SwiftUI
+
+@main
+struct InfectionSpreadSimulatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
